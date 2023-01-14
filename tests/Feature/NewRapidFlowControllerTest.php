7c8c0d536e718542b7886caf78c058c39224aace
Amir-16 <greenfitter516@gmail.com>
2023-01-14 17:30:42 +0600

    flow test started

--- tests/Feature/NewRapidFlowControllerTest.php
+++ tests/Feature/NewRapidFlowControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,9 +14,21 @@ class NewRapidFlowControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+
+    use RefreshDatabase;
+    public function test_authenticate_user_can_view_rapidflow()
     {
-        $response = $this->get('/');
+         $user = User::factory()->create();
+
+        $response = $this->post('login', [
+            'email'=> $user->email,
+            'password'=>'password',
+
+        ]);
+
+        $this->assertAuthenticated();
+
+        $response = $this->get(route('rapid.flow.view'));
 
         $response->assertStatus(200);
     }
